f4c527137bf9e58d97f9078e6883d780bdcc4ef9
Nicodemus <main@klotz>
2023-11-11 22:42:38 +0100

    Perform an extensive redesign

---
+++ input.js
@@ -0,0 +1,126 @@
+let _input = {
+	"start": "[\"@I500@\",\"@I501@\"]",
+	"members": {
+		"@I500@": {
+			"name": "Mamidolin Nichtzwiebel",
+			"birth_date": "16.08.1987",
+			"death_date": "",
+			"image_path": "images/Mamidolin Nichtzwiebel 1987.jpg"
+		},
+		"@I501@": {
+			"name": "Donklopopi Frosch",
+			"birth_date": "11.10.1982",
+			"death_date": "",
+			"image_path": "images/Donklopopi Frosch 1982.jpg"
+		},
+		"@I502@": {
+			"name": "Fridolin Frosch",
+			"birth_date": "13.05.2010",
+			"death_date": "",
+			"image_path": "images/Fridolin Frosch 2010.jpg"
+		},
+		"@I503@": {
+			"name": "Frieda Frosch",
+			"birth_date": "12.12.2012",
+			"death_date": "",
+			"image_path": "images/Frieda Frosch 2012.jpg"
+		},
+		"@I504@": {
+			"name": "Omifroschi Quapp",
+			"birth_date": "1965",
+			"death_date": "",
+			"image_path": "images/Omifroschi Quapp 1965.jpg"
+		},
+		"@I505@": {
+			"name": "Opifroschi Kaul",
+			"birth_date": "1965",
+			"death_date": "",
+			"image_path": "images/Opifroschi Kaul 1965.jpg"
+		},
+		"@I506@": {
+			"name": "",
+			"birth_date": "",
+			"death_date": ""
+		},
+		"@I507@": {
+			"name": "",
+			"birth_date": "1964",
+			"death_date": ""
+		},
+		"@I508@": {
+			"name": "Lidofrim Frosch",
+			"birth_date": "1978",
+			"death_date": "",
+			"image_path": "images/Lidofrim Frosch 1978.jpg"
+		},
+		"@I509@": {
+			"name": "Froschi Schroff",
+			"birth_date": "1923",
+			"death_date": "",
+			"image_path": "images/Froschi Schroff 1923.jpg"
+		},
+		"@I510@": {
+			"name": "",
+			"birth_date": "",
+			"death_date": ""
+		}
+	},
+	"links": [
+		[
+			"@I500@",
+			"[\"@I500@\",\"@I501@\"]"
+		],
+		[
+			"@I501@",
+			"[\"@I500@\",\"@I501@\"]"
+		],
+		[
+			"[\"@I500@\",\"@I501@\"]",
+			"@I502@"
+		],
+		[
+			"[\"@I500@\",\"@I501@\"]",
+			"@I503@"
+		],
+		[
+			"[\"@I504@\",\"@I505@\"]",
+			"@I500@"
+		],
+		[
+			"@I504@",
+			"[\"@I504@\",\"@I505@\"]"
+		],
+		[
+			"@I505@",
+			"[\"@I504@\",\"@I505@\"]"
+		],
+		[
+			"[\"@I506@\",\"@I507@\"]",
+			"@I501@"
+		],
+		[
+			"@I506@",
+			"[\"@I506@\",\"@I507@\"]"
+		],
+		[
+			"@I507@",
+			"[\"@I506@\",\"@I507@\"]"
+		],
+		[
+			"[\"@I506@\",\"@I507@\"]",
+			"@I508@"
+		],
+		[
+			"[\"@I509@\",\"@I510@\"]",
+			"@I504@"
+		],
+		[
+			"@I509@",
+			"[\"@I509@\",\"@I510@\"]"
+		],
+		[
+			"@I510@",
+			"[\"@I509@\",\"@I510@\"]"
+		]
+	]
+}
